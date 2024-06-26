b02cae4c3dbaaba7c11f901683fbbe764510e29a
chomikkombat <lemongmd@gmail.com>
2024-06-26 19:38:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-var count = document.getElementById(`count`)
-var click = document.getElementById(`click`)
+var count = document.getElementById(`count`);
+var click = document.getElementById(`click`);
 var counter = parseInt(sessionStorage.getItem(`count`));
 
+if (sessionStorage.getItem(`count`)) {
+    var counter = parseInt(sessionStorage.getItem(`count`));
+    count.textContent = counter
+}
+else {
+    var counter = 0
+}
+
 click.addEventListener(`click`, function(){
-    counter += 1;
+    counter += 1
     count.textContent = counter
     sessionStorage.setItem(`count`, counter);
 });
-
-window.addEventListener("load", function (event) {
-    count.textContent = counter
-  });
