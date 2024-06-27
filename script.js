324ac85d579897c3f4c38d69564e377e7a27a4c7
chomikkombat <lemongmd@gmail.com>
2024-06-27 22:08:29 +0700

    new update

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-var count = document.getElementById(`count`);
-var click = document.getElementById(`click`);
+var count = document.getElementById(`count`)
+const click = document.getElementById(`click`)
 var counter = parseInt(localStorage.getItem(`count`));
 
-if (localStorage.getItem(`count`)) {
+// сохранение
+if (sessionStorage.getItem(`count`)) {
     var counter = parseInt(localStorage.getItem(`count`));
     count.textContent = counter
 }
@@ -10,8 +11,20 @@ else {
     var counter = 0
 }
 
+// клик
 click.addEventListener(`click`, function(){
-    counter += 1
+    // анимация
+    click.classList.add('animate');
+    setTimeout(() => {
+        click.classList.remove('animate');
+    }, 100);
+    // ...
+    window.navigator.vibrate(200);
+    if (counter >= 500) {
+        counter += 2;
+    } else {
+        counter += 1;
+    }
     count.textContent = counter
     localStorage.setItem(`count`, counter);
 });
