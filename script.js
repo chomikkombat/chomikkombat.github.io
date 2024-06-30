9679029e5885d886bdc26633739cced44c22ac70
chomikkombat <lemongmd@gmail.com>
2024-06-30 18:20:55 +0700

    new update

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 var count = document.getElementById(`count`)
 const click = document.getElementById(`click`)
-var counter = parseInt(localStorage.getItem(`count`));
+var counter = parseInt(sessionStorage.getItem(`count`));
+var progressBar = document.getElementById(`progress-bar`)
+var notify = document.getElementById(`notify`)
+let upgrade = 1;
+
+notify.style.visibility = "hidden"
 
 // сохранение
 if (sessionStorage.getItem(`count`)) {
-    var counter = parseInt(localStorage.getItem(`count`));
-    count.textContent = counter
+    var counter = parseInt(sessionStorage.getItem(`count`));
+    count.textContent = counter;
 }
 else {
-    var counter = 0
+    var counter = 0;
 }
 
 // клик
-click.addEventListener(`click`, function(){
+click.addEventListener(`click`, function(event) {
     // анимация
     click.classList.add('animate');
     setTimeout(() => {
@@ -20,11 +25,16 @@ click.addEventListener(`click`, function(){
     }, 100);
     // ...
     window.navigator.vibrate(200);
-    if (counter >= 500) {
-        counter += 2;
-    } else {
-        counter += 1;
+    counter += upgrade;
+    if (counter % 1000 === 0) {
+        upgrade += 1;
+        notify.style.visibility = "visible"
+        notify.classList.add('animate');
+        setTimeout(() => {
+            notify.classList.remove('animate');
+            notify.style.visibility = "hidden"
+        }, 500);
     }
     count.textContent = counter
-    localStorage.setItem(`count`, counter);
+    sessionStorage.setItem(`count`, counter);
 });
