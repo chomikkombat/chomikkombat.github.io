febbbd26ffcb8829908abb12611869c600eee8b5
stysan <smirnovstas2011@gmail.com>
2024-07-03 19:08:03 +0300

    fix bug (full in description)
    fixed the bug where tapping the chomik wouldn't work for some people because there wasn't a closed bracket in the first if condition.
    
    also added consistent semicolons and removed and added blank lines here and there for better code readability

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
-var count = document.getElementById(`count`)
-var click = document.getElementById(`click`)
+var count = document.getElementById(`count`);
+var click = document.getElementById(`click`);
 var counter = parseInt(localStorage.getItem(`count`));
-var progressBar = document.getElementById(`progress-bar`)
-var notify = document.getElementById(`notify`)
+var progressBar = document.getElementById(`progress-bar`);
+var notify = document.getElementById(`notify`);
 let upgrade = 1;
 
-notify.style.visibility = "hidden"
+notify.style.visibility = "hidden";
 
 // сохранение
 
 if (localStorage.getItem(`count`)) {
-    var counter = parseInt(localStorage.getItem(`count`));
-    count.textContent = counter;
+  var counter = parseInt(localStorage.getItem(`count`));
+  count.textContent = counter;
+}
 
 if (localStorage.getItem(`count`)) {
-    var counter = parseInt(localStorage.getItem(`count`));
-    count.textContent = counter
-
-}
-else {
-    var counter = 0;
+  var counter = parseInt(localStorage.getItem(`count`));
+  count.textContent = counter;
+} else {
+  var counter = 0;
 }
 
 // клик
-click.addEventListener(`click`, function(event) {
-    // анимация
-    click.classList.add('animate');
+click.addEventListener('click', function(event) {
+  // анимация
+  click.classList.add('animate');
+  setTimeout(() => {
+      click.classList.remove('animate');
+  }, 100);
+  // ...
+  counter += upgrade;
+  if (counter % 1000 === 0) {
+    upgrade += 1;
+    notify.style.visibility = "visible";
+    notify.classList.add('animate');
     setTimeout(() => {
-        click.classList.remove('animate');
-    }, 100);
-    // ...
-    counter += upgrade;
-    if (counter % 1000 === 0) {
-        upgrade += 1;
-        notify.style.visibility = "visible"
-        notify.classList.add('animate');
-        setTimeout(() => {
-            notify.classList.remove('animate');
-            notify.style.visibility = "hidden"
-        }, 500);
-    }
-    count.textContent = counter
-    localStorage.setItem(`count`, counter);
-})};
+      notify.classList.remove('animate');
+      notify.style.visibility = "hidden";
+    }, 500);
+  }
+  count.textContent = counter;
+  localStorage.setItem(`count`, counter);
+});
+
+divElement.addEventListener("click", function() {
+    isClicked = true;
+    // Additional code to execute when the div is clicked
+});
